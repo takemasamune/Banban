b56b1df46e0fa35608e3ae6fad3cbf85aa06265b
Masashi Takeuchi <take@teki.to>
2019-08-25 21:06:50 +0900

    modified react hooks

--- src/App.js
+++ src/App.js
@@ -12,41 +12,53 @@ function App() {
     {id:7, url: 'assets/7.jpg'},
   ];
 
+  const findNextId = (currentid) => {
+    let newid = Math.floor(Math.random() * images.length) + 1;
+    if (currentid && newid === currentid) {
+      newid++;
+      if (newid > images.length) newid = 1;
+    }
+    return newid;
+  }
+
   const audio = new Audio('assets/ban.mp3');
-  const inputEl = React.useRef(null);
-  const [id, setId] = React.useState(2);
+  const [id, setId] = React.useState(findNextId());
   const [count, setCount] = React.useState(0);
-  let submitFlag = false;
+  const [isClicked, setClicked] = React.useState(false);
+  
+  const updateState = () => {
+    audio.play();
+    setId(findNextId(id));
+    setCount(count + 1);
+  }
+
+  const handleKeyDown = (e) => {
+    if (!isClicked) {
+      setClicked(true);
+      if (e.keyCode === 13 || e.target.value === 'imageElement') {
+        updateState();
+      }
+
+      setTimeout(() => {
+        setClicked(false);
+      }, 50);
+    }
+    else {
+      console.log("too busy, request rejected");
+    }
+  }
+  const handleTouchEvent = (e) => {
+    audio.load();
+  }
 
   React.useEffect(() => {
-    inputEl.current.focus();
-    document.addEventListener('keydown', onKeyPress);
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('touchstart', handleTouchEvent);
     return (() => {
-      document.removeEventListener('keydown', onKeyPress);
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('touchstart', handleTouchEvent);
     });
   });
-
-  const onKeyPress = function (e) {
-    if(!submitFlag) {
-      submitFlag = true;
-      e.preventDefault();
-      let newid = Math.floor(Math.random() * images.length) + 1;
-      if (newid === id) {
-        newid++;
-        if (newid > images.length) newid = 1;
-      }
-      if (e.keyCode === 13 || e.target.value === 'imageElement') {
-        setId(newid);
-        setCount(count + 1);
-        audio.play();
-      }
-      //console.log(e);
-      setInterval(() => {
-        submitFlag = false;
-      }, 150);
-    }
-    return false;
-  }
 
   const findImage = function(id) {
     return images.find((image) => { return image.id === id });
@@ -59,7 +71,7 @@ function App() {
         <h2>Count: {count}</h2>
       </header>
       <div className="Input">
-        <input type="image" className="App-image" ref={inputEl} value="imageElement" onClick={onKeyPress} src={findImage(id).url} alt="" />
+        <input type="image" className="App-image" value="imageElement" onClick={handleKeyDown} src={findImage(id).url} alt="" />
       </div>
     </div>
   );
